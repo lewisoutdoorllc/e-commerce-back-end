d4a72239d20e2787ad16324c3e7819fcc3826c06
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-16 21:47:45 -0400

    added stuff I needed and forgot to make branches

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,33 @@
-require('dotenv').config();
+// require('dotenv').config();
 
+// const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-
+require('dotenv').config()
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+    port: 3306,
+  }
+);
 module.exports = sequelize;
